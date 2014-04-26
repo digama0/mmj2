2ef95b2d865cc7ea6a10237a585588175a9c05c7
Lew Melnikov <lew.kaMelnikov@gmail.com>
2014-04-26 17:39:38 +0400

    Added BatchCommand class.

---
+++ src/mmj/util/BatchCommand.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package mmj.util;
+
+/**
+ * <p>
+ * This class contain batch command name and documentation of it and it's
+ * options. It's created to unite command name and it's documentation.
+ */
+public class BatchCommand {
+
+    public final String name;
+    public String documentation;
+
+    /**
+     * Constructor of BatchCommand. Sets documentation to null.
+     * 
+     * @param name_ name of the command that will be stored
+     */
+    BatchCommand(final String name_) {
+        name = name_;
+        documentation = null;
+    }
+
+    /**
+     * Constructor of BatchCommand.
+     * 
+     * @param name_ name of the command that will be stored
+     * @param documentation_ string with the documentation of this command
+     */
+    BatchCommand(final String name_, final String documentation_) {
+        name = name_;
+        documentation = documentation_;
+    }
+
+}
