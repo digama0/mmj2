ad2241d2728f815502f3082f3c550ad0fa86fe21
David A. Wheeler <dwheeler@dwheeler.com>
2020-01-14 19:33:04 -0500

    Add a batch file for Cygwin
    Add a new "mmj2-cygwin.bat" file to provide an easy way
    to invoke mmj2 when using Cygwin.
    
    Also, set its permssions, as well as mmj2.bat's, to allow
    execution. This doesn't matter from cmd.exe, but it
    *does* matter when creating a shortcut; Windows 10 won't run
    batch files as a shortcut unless they have permission to execute.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ mmj2jar/mmj2-cygwin.bat
@@ -0,0 +1,7 @@
+rem Use mmj2 and set.mm when they're inside the Cygwin home directory
+rem To set up, start up cygwin, and put set.mm in cygwin's ~/set.mm/ :
+rem  cd; git clone https://github.com/metamath/set.mm.git # DB in ~/set.mm/
+rem Then put mmj2 in cygwin's ~/mmj2 . To get the "master" version, use:
+rem  cd; git clone https://github.com/digama0/mmj2.git    # mmj2 in ~/mmj2
+rem Then use Windows "cmd" to run this command (e.g., with a shortcut!)
+java -Xincgc -Xms128M -Xmx512M -jar mmj2.jar RunParms.txt Y "" c:\cygwin64\home\%USERNAME%\set.mm ""
